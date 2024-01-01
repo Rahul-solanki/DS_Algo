0f6de955bc381fe6b8df1f52636a1ee1e38f0b8d
Rahul-solanki <rahulsolanki1227@gmail.com>
2024-01-01 09:57:21 +0530

    gitignore config

--- src/DivideAndConquer/MajorityElementLS.java
+++ src/DivideAndConquer/MajorityElementLS.java
@@ -2,6 +2,6 @@ package DivideAndConquer;
 
 public class MajorityElementLS {
     public static void main(String[] args) {
-        System.out.println();
+        System.out.println("Oven");
     }
 }
